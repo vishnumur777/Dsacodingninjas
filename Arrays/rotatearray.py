a94748add9d6b5596e5db668667f294828e9b271
vishnumur777 <varunushamurali@gmail.com>
2023-08-22 22:10:02 +0530

    Added .py in /home/hdd/gitauto/dsacodingninjas

---
+++ Arrays/rotatearray.py
@@ -0,0 +1,13 @@
+def rotateArray(arr,k):
+    for i in range(k):
+        arr.append(arr.pop(0))
+
+    return arr
+
+n=int(input())
+arr=list(map(int,input().split()))
+k=int(input())
+
+res=rotateArray(arr, k)
+print(*res)
+
