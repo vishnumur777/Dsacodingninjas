2f55e102717c8c5477a1c53a81e2c5180a6ed28d
vishnumur777 <varunushamurali@gmail.com>
2023-08-22 13:11:03 +0530

    Added .py in /home/hdd/gitauto/dsacodingninjas

---
+++ Arrays/kthlargestelement.py
@@ -0,0 +1,12 @@
+def getKthlargest(subsequence,k):
+    b = list(set(subsequence))
+    if len(b)==1:
+        return -1
+    subsequence.sort()
+    return subsequence[len(subsequence)-k]
+
+if __name__ == "__main__":
+    b=list(map(int,input().split()))
+    k=int(input())
+    v=getKthlargest(b,k)
+    print(f"The {k}th largest element from the given array is: {v}")
