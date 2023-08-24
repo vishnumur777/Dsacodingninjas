18fb34dbb9f624d557a0cd3a8093214738edf6c7
vishnumur777 <varunushamurali@gmail.com>
2023-08-24 10:40:00 +0530

    Added  in /home/hdd/gitauto/dsacodingninjas/Dynammic_programming

---
+++ Dynammic_programming/nthfibonacci.py
@@ -0,0 +1,16 @@
+def fibo(n):
+    memo = {}
+    if n in memo:
+        return memo[n]
+    if n<=2:
+        return n
+    else:
+        result = fibo(n-1)+fibo(n-2)
+
+    memo[n]=result
+    return result
+
+if __name__ == "__main__":
+    n=10
+    n1=50
+    print(fibo(n),fibo(n1))
---
+++ Dynammic_programming/nthfibonaccibottomup.py
@@ -0,0 +1,14 @@
+def fibo(n):
+    memo = {}
+    for i in range(1,n+1):
+        if i<=2:
+            result=1
+        else:
+            result=memo[i-1]+memo[i-2]
+        memo[i]=result
+
+    return memo[n]
+
+if __name__ == "__main__":
+    n=int(input())
+    print(fibo(n))
